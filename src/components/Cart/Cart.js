45b607859723f511291e46ca3fcdf3e394e6d1b8
sdiawa <sekoukaderisc@yahoo.fr>
2020-06-03 00:48:02 +0200

    calcule des sommes panier

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,7 +4,7 @@ import CartColumns from "./CartColumns";
 import EmptyCart from "./EmptyCart";
 import {ProductConsumer} from "../../context";
 import CartList from "./CartList";
-
+import CartTotals from "./CartTotals"
 
 export default class Cart extends Component{
     render() {
@@ -19,6 +19,7 @@ export default class Cart extends Component{
                                     <Title name="votre" title="panier"/>
                                     <CartColumns/>
                                     <CartList value={value}/>
+                                    <CartTotals value={value}/>
                                 </React.Fragment>
                             );
                         }
