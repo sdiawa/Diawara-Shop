fd1c7764c0c0728114a134f160a0e5ffe677daae
sdiawa <sekoukaderisc@yahoo.fr>
2020-06-03 03:44:11 +0200

    integration paypal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -19,7 +19,8 @@ export default class Cart extends Component{
                                     <Title name="votre" title="panier"/>
                                     <CartColumns/>
                                     <CartList value={value}/>
-                                    <CartTotals value={value}/>
+                                    <CartTotals value={value} history=
+                                        {this.props.history}/>
                                 </React.Fragment>
                             );
                         }
